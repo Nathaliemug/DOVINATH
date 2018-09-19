6bed8cf2cf956dcd95ae0948590b2697aff855f8
Nathaliemug <42577410+Nathaliemug@users.noreply.github.com>
2018-09-19 12:27:34 +0200

    initiation

---
+++ Nath/public_html/js/new.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".mobile-icon").click(function () {
+        $(".main-menu").toggle();
+        $(".mobile-icon").toggleClass("fa-bars fa-times");
+
+    });
+});
+    
+
+
