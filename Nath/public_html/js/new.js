ef42a1460e4135188bf9dda72c3330ead13f6861
Nathaliemug <42577410+Nathaliemug@users.noreply.github.com>
2018-09-21 13:40:40 +0200

    HTML ET CSS SUR CONTENU PARAGRAPHE

--- Nath/public_html/js/new.js
+++ Nath/public_html/js/new.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
+
+    $(".container-fluid .navigator .ecole-solidaire").addClass("animate");
     $(".mobile-icon").click(function () {
         $(".main-menu").toggle();
         $(".mobile-icon").toggleClass("fa-bars fa-times");
         $(".para1").css("left");
-        
+
+        $(".mobile-icon").click(function () {
+            $(".menu").toggle();
+            $(".mobile-icon").toggleClass("fa-bars fa-times");
+        });
     });
 });
-    
+
 
 
